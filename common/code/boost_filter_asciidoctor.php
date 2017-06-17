fbca2bafcbeb79d933b79a7ffba4df007016c456
Daniel James <daniel@calamity.org.uk>
2017-06-17 13:45:36 +0100

    Quick hack to handle asciidoctor docs

---
+++ common/code/boost_filter_asciidoctor.php
@@ -0,0 +1,36 @@
+<?php
+/*
+  Copyright 2005-2008 Redshift Software, Inc.
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+class BoostFilterAsciidoctor extends BoostFilter
+{
+    function echo_filtered()
+    {
+        $text = $this->content;
+
+        $match = null;
+
+        if(preg_match('@(?:</head>\s*)?<body[^>]*>@is', $text, $match, PREG_OFFSET_CAPTURE)) {
+            $is_xhtml = preg_match('@<!DOCTYPE[^>]*xhtml@i', $match[0][0]);
+            $tag_end = $is_xhtml ? '/>' : '>';
+
+            echo $this->alter_title(substr($text, 0, $match[0][1]));
+            echo '<link rel="icon" href="/favicon.ico" type="image/ico"'.$tag_end;
+            echo '<link rel="stylesheet" type="text/css" href="/style-v2/section-basic.css"'.$tag_end;
+            echo '<style>';
+            echo '#boost-common-heading-doc { position: static; }';
+            echo '#boost-common-heading-doc-spacer { display: none; }';
+            echo '</style>';
+            echo $match[0][0];
+            virtual("/common/heading-doc.html");
+            echo latest_link($this->data);
+            echo $this->prepare_html($this->remove_html_banner(substr($text, $match[0][1] + strlen($match[0][0]))));
+        }
+        else {
+            echo $text;
+        }
+    }
+}
