726130b789b77c3bee535b54217f104e00dbb54f
Daniel James <daniel@calamity.org.uk>
2017-06-17 13:59:52 +0100

    Simplify the asciidoctor filter a little

--- common/code/boost_filter_asciidoctor.php
+++ common/code/boost_filter_asciidoctor.php
@@ -14,12 +14,9 @@ class BoostFilterAsciidoctor extends BoostFilter
         $match = null;
 
         if(preg_match('@(?:</head>\s*)?<body[^>]*>@is', $text, $match, PREG_OFFSET_CAPTURE)) {
-            $is_xhtml = preg_match('@<!DOCTYPE[^>]*xhtml@i', $match[0][0]);
-            $tag_end = $is_xhtml ? '/>' : '>';
-
             echo $this->alter_title(substr($text, 0, $match[0][1]));
-            echo '<link rel="icon" href="/favicon.ico" type="image/ico"'.$tag_end;
-            echo '<link rel="stylesheet" type="text/css" href="/style-v2/section-basic.css"'.$tag_end;
+            echo '<link rel="icon" href="/favicon.ico" type="image/ico">';
+            echo '<link rel="stylesheet" type="text/css" href="/style-v2/section-basic.css">';
             echo '<style>';
             echo '#boost-common-heading-doc { position: static; }';
             echo '#boost-common-heading-doc-spacer { display: none; }';
@@ -27,7 +24,7 @@ class BoostFilterAsciidoctor extends BoostFilter
             echo $match[0][0];
             virtual("/common/heading-doc.html");
             echo latest_link($this->data);
-            echo $this->prepare_html($this->remove_html_banner(substr($text, $match[0][1] + strlen($match[0][0]))));
+            echo $this->prepare_html(substr($text, $match[0][1] + strlen($match[0][0])));
         }
         else {
             echo $text;
